16cf163363189c3e5784fbb8d950a47c2ed4bb5f
Dominic <18anthonyd@badinhs.org>
2016-10-05 08:25:34 -0400

    Added Segue and Navigation

--- MyFavoriteEmojis/ViewController.swift
+++ MyFavoriteEmojis/ViewController.swift
@@ -37,6 +37,10 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier:"moveSegue", sender: "🌞")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
